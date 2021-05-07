8e43810b5fe636cc284df78690e05aec01fb7cfc
vaishviluhar <78733046+vaishviluhar@users.noreply.github.com>
2021-05-07 14:04:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 var stand;
 var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16;
 var polygon;
